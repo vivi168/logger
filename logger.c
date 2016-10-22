96bd0b3cdc87aa7e523623a39fb6a3bfb7e5e6bb
vivien <vivienbihl@gmail.com>
2016-10-22 14:41:02 +0200

    order of calls

--- logger.c
+++ logger.c
@@ -6,26 +6,27 @@
 
 void logger(char *file, char *fmt, ...)
 {
-        va_list ap;
-        va_start(ap, fmt);
-        char msg[LOGSIZ];
-        vsnprintf(msg, sizeof(msg), fmt, ap);
-
-        time_t rawtime;
-        struct tm *timeinfo;
-        time(&rawtime);
-        timeinfo = localtime(&rawtime);
-        char buf[TIMSIZ];
-
-        strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", timeinfo);
-
         FILE *f = fopen(file, "a");
         if (f == NULL) {
                 printf("Error opening file\n");
                 exit(1);
         }
+
+        va_list ap;
+        char msg[LOGSIZ];
+        time_t rawtime;
+        struct tm *timeinfo;
+
+        va_start(ap, fmt);
+        vsnprintf(msg, sizeof(msg), fmt, ap);
+        va_end(ap);
+
+        time(&rawtime);
+        timeinfo = localtime(&rawtime);
+        char buf[TIMSIZ];
+        strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", timeinfo);
+
         fprintf(f, "[%s]: %s\n", buf, msg);
 
         fclose(f);
-        va_end(ap);
 }
